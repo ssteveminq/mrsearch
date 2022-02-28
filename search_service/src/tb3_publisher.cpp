ba9a70c1981d77d73f00ed1aacdb34032ecd859f
Ryan Gupta <ryangupta8@gmail.com>
2022-02-28 00:58:54 -0600

    Added publishers for ground truth in addition to amcl

---
+++ search_service/src/tb3_publisher.cpp
@@ -0,0 +1,60 @@
+// ROS Modules
+#include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <std_msgs/Header.h>
+
+
+
+using namespace std;
+
+class Listener{
+public:
+    geometry_msgs::PoseWithCovariance pose;
+    std_msgs::Header header;
+
+    void loc_cb(const nav_msgs::Odometry::ConstPtr& msg)
+    {
+        header = msg->header;
+        pose = msg->pose;
+    }
+};
+
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "tb3_pose_publisher_node");
+  ros::NodeHandle n;
+  ros::Rate loop_rate(500);
+
+  Listener listener;
+
+  ros::Subscriber tb3_loc_sub = n.subscribe("/tb3/odom", 1000, &Listener::loc_cb, &listener);
+  ros::Publisher tb3_pose_pub = n.advertise<geometry_msgs::PoseWithCovarianceStamped>("/tb3/pose", 1000);
+
+  // To publish 
+  geometry_msgs::PoseWithCovarianceStamped tb3_pose_msg;
+
+  bool first_time = true;
+
+  ros::Time current_time, last_time;
+  current_time = ros::Time::now();
+  last_time = ros::Time::now();
+
+  while (ros::ok()){
+    
+    current_time = ros::Time::now();
+
+    tb3_pose_msg.header.stamp = current_time;
+    tb3_pose_msg.pose = listener.pose;
+    tb3_pose_msg.header.frame_id = "map";
+
+    tb3_pose_pub.publish(tb3_pose_msg);
+        
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
