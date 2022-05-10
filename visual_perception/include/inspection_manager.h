b67df54a5c80e92fc43a22ffea6be87b9393616e
ssteveminq <steveminq@gmail.com>
2022-05-10 02:20:15 -0500

    hsr_inspection_manager added & action client added

--- visual_perception/include/inspection_manager.h
+++ visual_perception/include/inspection_manager.h
@@ -40,7 +40,7 @@ class inspection_manager
 {
 public:
   /// constructor
-  inspection_manager(ros::NodeHandle nh, std::string pose_topic_, std::string camera_frame);
+  inspection_manager(ros::NodeHandle nh);
 
   /// destructor
   virtual ~inspection_manager();
@@ -48,9 +48,12 @@ public:
   /// callback for messages
 
   void publish_cameraregion();
+  void publish_inspection_poses();
   //void global_pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg);
   void global_pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
   void scaled_static_map_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg);
+  void search_map_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg);
+  void posearry_callback(const geometry_msgs::PoseArray::ConstPtr& msg);
   
   bool Comparetwopoistions(std::vector<double> pos,std::vector<double> pos2,double criterion);
   bool check_staticObs(float x_pos,float y_pos);
@@ -60,6 +63,12 @@ public:
 
   int  globalcoord_To_SScaled_map_index(float x_pos,float y_pos);
   int  CoordinateTransform_Global2_staticMap(float global_x, float global_y);
+  int Coord2CellNum(double _x, double _y, const nav_msgs::OccupancyGrid& inputmap_);
+  bool checked_with_search_map(const geometry_msgs::Pose& input );
+  bool near_from_poses(const geometry_msgs::Pose& input );
+
+  void agent1_localmap_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg);
+  void update_occ_grid_map(const nav_msgs::OccupancyGridConstPtr& msg);
     
   void getCameraregion();
   void spin();
@@ -69,12 +78,15 @@ private:
 
   ros::NodeHandle nh_;
 
-  ros::Publisher camera_visible_region_pub;
-  ros::Publisher human_target_pub;
+  ros::Publisher viz_pub;
+  ros::Publisher inspect_target_pub;
 
+  ros::Subscriber stfpose_sub;
   ros::Subscriber globalpose_sub;
   ros::Subscriber Scaled_static_map_sub;
   ros::Subscriber joint_state_sub;
+  ros::Subscriber search_map_sub;
+  ros::Subscriber agent_map_sub;
   
   // tf listener
   tf::TransformListener     robot_state_;
@@ -86,13 +98,19 @@ private:
   std::vector<int> visiblie_idx_set;
 
   std::vector<double> global_pose;
+  geometry_msgs::PoseArray pose_array;
+  geometry_msgs::PoseArray filtered_pose_array;
 
   int num_of_detected_human;
   nav_msgs::OccupancyGrid Scaled_map;
   nav_msgs::OccupancyGrid camera_visible_region;
+  nav_msgs::OccupancyGrid cur_search_map;
+  nav_msgs::OccupancyGrid viz_search_map;
+  nav_msgs::OccupancyGrid agent1_local_map;
 
   std::string agent_pose_topic;
   std::string camera_frame;
+  bool search_map_received;
   
   
 
--- visual_perception/src/inspection_manager.cpp
+++ visual_perception/src/inspection_manager.cpp
@@ -15,26 +15,40 @@ static const double       tracker_init_dist          = 4.0;
 bool IsNotInitilized = true;
 
 // constructor
-inspection_manager::inspection_manager(ros::NodeHandle nh, std::string pose_topic_="amcl_pose", std::string camera_frame_="spot/camera_link")
-  : nh_(nh),
+inspection_manager::inspection_manager(ros::NodeHandle nh)
+  : nh_(nh),search_map_received(true),
     robot_state_()
 {
 
-  agent_pose_topic = pose_topic_;
-  camera_frame=camera_frame_;
-  cout<<"agent_pose_toic: "<<agent_pose_topic <<std::endl;
-  cout<<"CAMERA_frame: "<<camera_frame<<std::endl;
-  camera_visible_region_pub=nh_.advertise<nav_msgs::OccupancyGrid>("/camera_region_map", 10, true);
-  globalpose_sub=nh_.subscribe<geometry_msgs::PoseWithCovarianceStamped>(agent_pose_topic,10,&inspection_manager::global_pose_callback,this);
+  //agent_pose_topic = pose_topic_;
+  //camera_frame=camera_frame_;
+  //cout<<"agent_pose_toic: "<<agent_pose_topic <<std::endl;
+  //cout<<"CAMERA_frame: "<<camera_frame<<std::endl;
+  viz_pub=nh_.advertise<nav_msgs::OccupancyGrid>("/viz_search_map", 10, true);
+  //globalpose_sub=nh_.subscribe<geometry_msgs::PoseWithCovarianceStamped>(agent_pose_topic,10,&inspection_manager::global_pose_callback,this);
+  inspect_target_pub=nh_.advertise<geometry_msgs::PoseArray>("/inspection_poses", 10, true);
+  stfpose_sub=nh_.subscribe<geometry_msgs::PoseArray>("/stf_poses",10,&inspection_manager::posearry_callback,this);
+  search_map_sub=nh_.subscribe<nav_msgs::OccupancyGrid>("/search_map",10,&inspection_manager::search_map_callback,this);
+  agent_map_sub=nh_.subscribe<nav_msgs::OccupancyGrid>("hsrb/fov_map",10,&inspection_manager::agent1_localmap_callback,this);
   global_pose.resize(3,0.0);
 
   //camera region
-  camera_visible_region.info.width=30;
-  camera_visible_region.info.height= 30;
+  camera_visible_region.info.width=20;
+  camera_visible_region.info.height= 20;
   camera_visible_region.info.resolution=0.5;
-  camera_visible_region.info.origin.position.x=-7.5;
-  camera_visible_region.info.origin.position.y=-7.5;
+  camera_visible_region.info.origin.position.x=-5.0;
+  camera_visible_region.info.origin.position.y=-5.0;
   camera_visible_region.data.resize(camera_visible_region.info.width*camera_visible_region.info.height,0.0);
+
+  //camera region
+  viz_search_map.info.width=100;
+  viz_search_map.info.height= 100;
+  viz_search_map.info.resolution=0.5;
+  viz_search_map.info.origin.position.x=-25.0;
+  viz_search_map.info.origin.position.y=-25.0;
+  viz_search_map.data.resize(viz_search_map.info.width*viz_search_map.info.height,0.0);
+
+
 
 }
 
@@ -50,13 +64,44 @@ inspection_manager::~inspection_manager()
 void inspection_manager::publish_cameraregion()
 {
 
-   getCameraregion();
-   camera_visible_region.header.stamp =  ros::Time::now();
-   camera_visible_region.header.frame_id = "map"; 
-   camera_visible_region_pub.publish(camera_visible_region);
-
-
+   //getCameraregion();
+   viz_search_map.header.stamp =  ros::Time::now();
+   viz_search_map.header.frame_id = "map"; 
+   viz_pub.publish(viz_search_map);
 }
+
+void inspection_manager::publish_inspection_poses()
+{
+    filtered_pose_array.poses.clear();
+    bool isnear=false;
+    bool issearched=false;
+    for(int i(0);i< pose_array.poses.size();i++)
+    {
+        //if(!near_from_poses(ps) && !checked_with_search_map(ps))
+        isnear = near_from_poses(pose_array.poses[i]);
+        if(!isnear)
+        {
+            issearched=checked_with_search_map(pose_array.poses[i]);
+        }
+
+
+
+        //cout<<i<<" -th pose"<<"near?"<<isnear<<endl;
+        if(!isnear && !issearched)
+        {
+            filtered_pose_array.poses.push_back(pose_array.poses[i]);
+            //cout<<"push_back"<<endl;
+        }
+    }
+
+   //getCameraregion();
+   filtered_pose_array.header.stamp =  ros::Time::now();
+   filtered_pose_array.header.frame_id = "map_en"; 
+   //filtered_pose_array.header = pose_array.header;
+   inspect_target_pub.publish(filtered_pose_array);
+   //filtered_pose_array.poses.clear();
+}
+
 
 
 void inspection_manager::getCameraregion()
@@ -111,6 +156,35 @@ void inspection_manager::getCameraregion()
 
 
 
+}
+
+void inspection_manager::agent1_localmap_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
+{
+    agent1_local_map=*msg;
+    update_occ_grid_map(msg);
+}
+
+void inspection_manager::update_occ_grid_map(const nav_msgs::OccupancyGridConstPtr& msg)
+{
+    //origin_point_map_en =local_map.info.origin.position
+    double px, py =0.0;
+    int map_idx,search_idx =0;
+
+    for(int j(0); j< msg->info.height;j++)
+        for(int i(0); i< msg->info.width;i++)
+        {
+            map_idx = j*msg->info.width+i;
+            px = msg->info.origin.position.x+(i+0.5)*msg->info.resolution;
+            py = msg->info.origin.position.y+(j+0.5)*msg->info.resolution;
+            search_idx = Coord2CellNum(px,py, viz_search_map);         // get search cell idx 
+            //Update searchmap according to local measurement
+            // if local_map is known (occ or free) and global_map is not occupied by static obstacle 
+            if(search_idx<viz_search_map.data.size())
+            {
+                if(msg->data[map_idx]>0) 
+                    viz_search_map.data[search_idx]=30;
+            }
+        }
 }
 
 
@@ -186,7 +260,6 @@ bool inspection_manager::getlinevalue(int line_type,double input_x, double input
         coeff_sign=1.0; 
       else if(isspecial==3)
         coeff_sign=1.0;
-           
 
       break;
   default:
@@ -203,7 +276,17 @@ bool inspection_manager::getlinevalue(int line_type,double input_x, double input
 
 }
 
-
+void inspection_manager::search_map_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
+{
+    cur_search_map=*msg;
+    if(!search_map_received)
+    {
+        viz_search_map.info = msg->info;
+        viz_search_map.header.frame_id = msg->header.frame_id;
+        viz_search_map.data.resize(msg->info.width*msg->info.height,0.0);
+        search_map_received=true;
+    }
+}
 
 
 void inspection_manager::scaled_static_map_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
@@ -300,6 +383,118 @@ double inspection_manager::getDistance_from_Vec(std::vector<double> origin, doub
   return temp;
 
 }
+int inspection_manager::Coord2CellNum(double _x, double _y, const nav_msgs::OccupancyGrid& inputmap_)
+{	
+    //ROS_INFO("x: %.2lf, y: %.2lf", _x, _y);
+    std::vector<int> target_Coord;
+    target_Coord.resize(2,0);
+
+    double  temp_x  = _x-inputmap_.info.origin.position.x;
+    double  temp_y = _y-inputmap_.info.origin.position.y;
+
+    target_Coord[0]= (int) floor(temp_x/inputmap_.info.resolution);
+    target_Coord[1]= (int) floor(temp_y/inputmap_.info.resolution);
+
+    int index= target_Coord[0]+inputmap_.info.width*target_Coord[1];
+    return index;
+}
+
+
+
+bool inspection_manager::checked_with_search_map(const geometry_msgs::Pose& input )
+{
+
+    listener.waitForTransform("map_en", "map", ros::Time(0), ros::Duration(2.0));
+    geometry_msgs::PoseStamped pose_in;     //added by mk, ryan
+    geometry_msgs::PoseStamped pose_out;     //added by mk, ryan
+      //pose_in.header.stamp=ros::Time(0);
+    pose_in.header.stamp=ros::Time().now();
+    pose_in.header.frame_id="map_en";
+    pose_in.pose=input;
+    pose_in.pose.orientation.x=0.0;
+    pose_in.pose.orientation.y=0.0;
+    pose_in.pose.orientation.z=0.0;
+    pose_in.pose.orientation.w=1.0;
+
+    try{
+        listener.transformPose("map", ros::Time(0), pose_in, pose_in.header.frame_id, pose_out);
+        pose_out.pose.orientation.w=1;
+        //return true;
+      }catch(tf::TransformException ex){
+        std::cout << ex.what() << std::endl;
+        sleep(0.1);
+        cout<<"here-false"<<endl;
+        return false;
+   }
+
+    if(search_map_received)
+    {
+        auto search_idx = Coord2CellNum(pose_out.pose.position.x,pose_out.pose.position.y, viz_search_map);         // get search cell idx 
+        //cout<<"pos_x:"<<pose_out.pose.position.x <<pose_out.pose.position.y<<endl;
+        //cout<<"search_idx"<<search_idx<<endl;
+        if(search_idx<viz_search_map.data.size())
+        {
+            if(viz_search_map.data[search_idx]>0) 
+                return true;
+            //if already that pose is expored or static obstacles;
+            //return false;
+        }
+        else
+            return true;
+    }
+
+    return false;
+
+    
+   //pose_en ==> pose
+   //
+   //for(auto epose: pose_array.poses)
+    //{
+        //filter with existing poses
+        //if(sqrt(pow(input.position.x-epose.position.x,2)+pow(input.position.y-epose.position.y))<0.25);
+            //return true;
+    //}
+    //return false;
+}
+
+bool inspection_manager::near_from_poses(const geometry_msgs::Pose& input )
+{
+
+    double dist_threshold = 0.4;
+    //cout<<"filtered_pose_array.size(): "<<filtered_pose_array.poses.size()<<endl;;
+    for(auto& epose: filtered_pose_array.poses)
+    {
+        //filter with existing poses
+        double dist = sqrt(pow(input.position.x-epose.position.x,2)+pow(input.position.y-epose.position.y,2));
+        //cout<<"dist: "<<dist<<endl;
+        if(dist<0.4)
+        {
+            //cout<<"input_x"<<input.position.x<<", y: "<<input.position.y<<", epose: "<<epose.position.x<<", "<<epose.position.y<<endl;
+            return true;
+        }
+            //cout<<"not here"<<endl;
+
+    }
+    //cout<<"return false"<<endl;
+
+    return false;
+}
+
+
+void inspection_manager::posearry_callback(const geometry_msgs::PoseArray::ConstPtr& msg)
+{
+    //pose_array.header = msg->header;
+    pose_array = *msg;
+    //bool isnear=false;
+    //for(auto& ps : msg->poses)
+    //{
+        //if(!near_from_poses(ps) && !checked_with_search_map(ps))
+        //isnear = near_from_poses(ps);
+        //if(!isnear)
+            //pose_array.poses.push_back(ps);
+    //}
+}
+
 
 void inspection_manager::global_pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
 {
@@ -364,19 +559,19 @@ bool inspection_manager::Comparetwopoistions(std::vector<double> pos,std::vector
 // filter loop
 void inspection_manager::spin()
 {
-  ROS_INFO("People tracking manager started.");
+  ROS_INFO("inspection_manager started.");
 
   while (ros::ok())
   {
-    publish_cameraregion();
-    //Publish_nav_target();
     // ------ LOCKED ------
     boost::mutex::scoped_lock lock(filter_mutex_);
+    publish_cameraregion();
+    publish_inspection_poses();
     lock.unlock();
     // ------ LOCKED ------
 
     // sleep
-    ros::Duration(0.4).sleep();
+    ros::Duration(0.25).sleep();
 
     ros::spinOnce();
   }
@@ -392,13 +587,13 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "inpsection_manager_node");
   ros::NodeHandle(nh);
 
-  std::cout<<"pose_topic: "<<argv[1]<<std::endl;
-  std::cout<<"camera_frame: "<<argv[2]<<std::endl;
+  //std::cout<<"pose_topic: "<<argv[1]<<std::endl;
+  //std::cout<<"camera_frame: "<<argv[2]<<std::endl;
   // create tracker node
-  inspection_manager visible_region_node(nh, argv[1],argv[2]);
+  inspection_manager inspection_node(nh);
 
   // wait for filter to finish
-  visible_region_node.spin();
+  inspection_node.spin();
   // Clean up
 
   return 0;
