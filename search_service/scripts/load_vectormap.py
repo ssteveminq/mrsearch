1d944d60b23a0367c1fb91f3881318059add0018
Ryan Gupta <ryangupta8@gmail.com>
2022-07-18 19:17:49 -0500

    file for reading vectormap and publishing as a path. Also added large interior objects near edges

---
+++ search_service/scripts/load_vectormap.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python
+
+import rospy
+import actionlib
+import numpy as np
+import os
+import sys
+import nav_msgs.msg
+import geometry_msgs.msg
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped, Pose
+
+
+
+_FRAME_ID = 'map'
+_FILENAME = '/home/knapsack/workspaces/test_ws/src/mrsearch/tmp/vectormaps/ahg-alley.vectormap.txt'
+
+
+def vectormap_manager():    
+    msg = Path()
+    msg.header.frame_id = _FRAME_ID
+    msg.header.stamp = rospy.Time.now()
+
+    vectormap_pub=rospy.Publisher('/vectormap',Path,queue_size=10)
+
+    map_txt = open(_FILENAME, 'r')
+
+    for line in map_txt:
+        pose = PoseStamped()
+        # line = map_txt.readline()
+        # if not line:
+            # print('break')
+            # break
+        values = line.split(',')
+        # print(values[0])
+        # print(values[1])
+        pose.pose.position.x = float(values[0])
+        pose.pose.position.y = float(values[1])
+        pose.pose.position.z = 0.0
+        pose.pose.orientation.x = 0.
+        pose.pose.orientation.y = 0.
+        pose.pose.orientation.z = 0.
+        pose.pose.orientation.w = 1.
+        msg.poses.append(pose)
+
+    map_txt.close()
+    rospy.loginfo("Publishing Vectormap...")
+
+    while not rospy.is_shutdown():
+        vectormap_pub.publish(msg)
+
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('vectormap_manager')
+    vectormap_manager()
+    # mains()
